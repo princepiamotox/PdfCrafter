17ac6f08a8b94f93e2761e0943e8105d1b38b913
Princepia <princepia.motox@gmail.com>
2026-09-02 23:01:19 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE = 'pdfcrafter-v1';
-const CORE = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting())));
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-  if (url.origin === location.origin) e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
